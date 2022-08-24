23b980b3e392de3fbd3d75ab3e786d38e584e761
Augusto <augusto.94.castro@gmail.com>
2022-08-24 10:30:58 -0300

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
